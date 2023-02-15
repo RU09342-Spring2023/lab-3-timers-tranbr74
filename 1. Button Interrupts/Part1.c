7f68c04394b790352a456e4cbcba6e3e18206bb3
Brandon Tran <tranbr74@students.rowan.edu>
2023-02-15 14:16:22 -0500

    Update

--- 1. Button Interrupts/Part1.c
+++ 1. Button Interrupts/Part1.c
@@ -2,13 +2,14 @@
  * Part1.c
  *
  *  Created on: Feb 11, 2023
- *      Author: Russell Trafford
+ *      Author: Brandon Tran
  *
  *  This code is a template which will change the color of the LED being blinked using the interrupt routine.
  */
 
 
 #include <msp430.h>
+
 
 char LED_Color = 0x01;                       // Global Variable to determine which LED should be blinking
 
@@ -66,7 +67,7 @@ void gpioInit(){
       // Configure Button on P2.3 as input with pullup resistor
       P2OUT |= BIT3;                          // Configure P2.3 as pulled-up
       P2REN |= BIT3;                          // P2.3 pull-up register enable
-      P2IES &= ~BIT3;                         // P2.3 Low --> High edge
+      P2IES |= BIT3;                          // Toggle rising and falling edge.
       P2IE |= BIT3;                           // P2.3 interrupt enabled
 
 }
@@ -79,17 +80,7 @@ void gpioInit(){
 __interrupt void Port_2(void)
 {
     P2IFG &= ~BIT3;                         // Clear P1.3 IFG
-
-    if ( )       // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a rising edge.
-    {
-        LED_Color = 0;
-        // @TODO Add code to change which edge the interrupt should be looking for next
-    }
-
-    else if ( ) // @TODO Fill in this argument within the If statement to check if the interrupt was triggered off a falling edge.
-    {
-        LED_Color = 1;
-        // @TODO Add code to change which edge the interrupt should be looking for next
-    }
+    LED_Color ^= BIT0;                 // Toggle LED_Color
+    P2IES = ~P2IES;                          // Toggle rising and falling edge.
 }
 
